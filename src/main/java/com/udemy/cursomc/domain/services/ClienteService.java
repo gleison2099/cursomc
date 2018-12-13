6bac6bd303d2d29b9711cc0f5eb316be8c77e049
Gleison Franca <gleison2099@gmail.com>
2018-12-13 14:45:51 -0200

    endpoit /clientes/{id} disponivel

---
+++ src/main/java/com/udemy/cursomc/domain/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.udemy.cursomc.domain.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemy.cursomc.domain.Cliente;
+import com.udemy.cursomc.domain.services.exception.ObjectNotFoundException;
+import com.udemy.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado Id: " + id +
+				", Tipo: " + Cliente.class.getName()));
+
+	}
+
+}
