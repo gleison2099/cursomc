08e8de8509bb4132f5b05829c0fae53ebdd061c9
Gleison Franca <gleison2099@gmail.com>
2018-12-17 16:19:37 -0200

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/udemy/cursomc/domain/PagamentoComBoleto.java
@@ -0,0 +1,46 @@
+package com.udemy.cursomc.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import com.udemy.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComBoleto extends Pagamento {
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Date dataVencimento;
+	private Date dataPagamento;
+	
+	public PagamentoComBoleto() {
+	}
+	
+	public PagamentoComBoleto(Integer id, EstadoPagamento estadoPagamento,
+			                  Pedido pedido, Date dataVencimento, Date dataPagamento) {
+		super(id, estadoPagamento, pedido);
+		this.setDataVencimento(dataVencimento);
+		this.setDataPagamento(dataPagamento);
+		
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+	
+	
+
+}
